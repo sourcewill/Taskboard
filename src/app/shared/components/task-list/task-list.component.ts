3077116474104510291da6e35896d29fb7139f33
William Rodrigues <email.williamrodrigues@gmail.com>
2022-06-24 15:16:17 -0300

    Save task list on local storage.

--- src/app/shared/components/task-list/task-list.component.ts
+++ src/app/shared/components/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Task } from '../../models/task.model';
+import { setTaskList } from '../../store/actions/data.actions';
 import { selectTaskList } from '../../store/selectors/data.selectors';
 
 @Component({
@@ -12,12 +13,27 @@ export class TaskListComponent implements OnInit {
 
   taskList: Task[] = [];
 
-  constructor(private store: Store) { }
-
-  ngOnInit(): void {
-    this.store.select(selectTaskList).subscribe(data => this.taskList = data);
+  constructor(private store: Store) { 
+    this.restoreDataFromLocalStorage();
   }
 
+  ngOnInit(): void {
+    this.store.select(selectTaskList).subscribe(data => {
+      this.taskList = data;
+      this.saveDataOnLocalStorage();
+    });
+  }
 
+  saveDataOnLocalStorage(){
+    localStorage.setItem('taskList', JSON.stringify(this.taskList));
+  }
+
+  restoreDataFromLocalStorage(){
+    let localDataTaskList: string | null = localStorage.getItem('taskList');
+    if(localDataTaskList){
+      let restoredTaskList: Task[] = JSON.parse(localDataTaskList);
+      this.store.dispatch(setTaskList({payload: restoredTaskList}));
+    }
+  }
 
 }
